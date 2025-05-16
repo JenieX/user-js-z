00b877ba75bbe4fde88e96bd0b5d7a1d53fb5c0d
JenieX <sozorablack@gmail.com>
2025-05-16 06:14:28 +0300

    chore: update to check updating functionality

--- fitgirl-safer-content/fitgirl-safer-content.user.js
+++ fitgirl-safer-content/fitgirl-safer-content.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           fitgirl-safer-content
-// @version        1.0.0
+// @version        1.0.1
 // @namespace      https://github.com/JenieX/user-js-next
 // @description    Remove games that are tagged as adult games.
 // @author         JenieX
